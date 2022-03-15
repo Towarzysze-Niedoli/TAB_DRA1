bb7ce88cd43579d0fb0685a72f0c0a3f17d83237
Piotr Rajnisz <rajniszp@gmail.com>
2022-03-15 17:03:40 +0100

    Entity modeling project creation

---
+++ ClinicManagementSystem/ClinicManagementSystem.Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClinicManagementSystem.Models
+{
+    public class Address
+    {
+        public int ID { get; set; }
+        public String City { get; set; }
+        public String Street { get; set; }
+        public String HomeNumber { get; set; }
+        public String ZipCode { get; set; } // TODO zmienic na klase zipcode?
+    }
+}
