7f1cff4777b65a472ed939060273e22b38f4a19a
AnnaJust <anna.just@op.pl>
2022-04-10 23:12:01 +0200

    entitymodeling-update

---
+++ ClinicManagementSystem/ClinicManagementSystem.Entities/Models/LabTechnician.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagementSystem.Entities.Models
+{
+    class LabTechnician : Person
+    {
+        public Address? Address { get; set; }
+    }
+}
---
+++ ClinicManagementSystem/ClinicManagementSystem.Entities/Models/LaboratoryManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace ClinicManagementSystem.Entities.Models
+{
+    class LaboratoryManager : Person
+    {
+        public Address? Address { get; set; }
+    }
+}
