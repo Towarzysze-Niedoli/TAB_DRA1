1982f353795db0f93cad1d330f4c087bf6dd0e0d
Konrad Gawelek <konradgawelek@gmail.com>
2022-05-06 17:43:33 +0200

    created appointment repository

---
+++ ClinicManagementSystem/ClinicManagementSystem.Repositories/Repositories/IAppointmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using ClinicManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagementSystem.Repositories.Repositories
+{
+    internal interface IAppointmentRepository: IDisposable
+    {
+        IEnumerable<Appointment> GetAppointments();
+        Appointment GetAppointmenttByID(int appointmentId);
+        void InsertAppointment(Appointment appointment);
+        void DeleteAppointment(int appointmentId);
+        void UpdateAppointment(Appointment appointment);
+        void Save();
+        void GetAppointmentForDoctor(Doctor doctor);
+    }
+}
---
+++ ClinicManagementSystem/ClinicManagementSystem.Repositories/Repositories/impl/AppointmentRepository.cs
@@ -0,0 +1,67 @@
+﻿using ClinicManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagementSystem.Repositories.Repositories.impl
+{
+    public class AppointmentRepository : IAppointmentRepository, IDisposable
+    {
+        public void DeleteAppointment(int appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+        public void GetAppointmentForDoctor(Doctor doctor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Appointment> GetAppointments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Appointment GetAppointmenttByID(int appointmentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAppointment(Appointment appointment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAppointment(Appointment appointment)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
