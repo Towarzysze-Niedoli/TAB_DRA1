8f3ca0733cf129a914fb008f4688b6ba37e2a665
Konrad Gawelek <konradgawelek@gmail.com>
2022-06-05 13:20:23 +0200

    examination CRUD

---
+++ ClinicManagementSystem/ClinicManagementSystem.Services/IExaminationService.cs
@@ -0,0 +1,17 @@
+﻿using ClinicManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagementSystem.Services
+{
+    internal interface IExaminationService
+    {
+        IEnumerable<Examination> GetExaminations();
+        Examination GetExaminationByID(int examinationId);
+        void InsertExamination(Examination examination);
+        void DeleteExamination(int examinationId);
+        void UpdateExamination(Examination examination);
+        void Save();
+    }
+}
---
+++ ClinicManagementSystem/ClinicManagementSystem.Services/impl/ExaminationService.cs
@@ -0,0 +1,72 @@
+﻿using ClinicManagementSystem.Entities;
+using ClinicManagementSystem.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicManagementSystem.Services.impl
+{
+    public class ExaminationService: IExaminationService, IDisposable
+    {
+        private ISystemContext context;
+
+        public ExaminationService(ISystemContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteExamination(int examinationId)
+        {
+            Examination examination = context.Examinations.Find(examinationId);
+            context.Examinations.Remove(examination);
+        }
+
+        
+
+        public Examination GetExaminationByID(int examinationId)
+        {
+            return context.Examinations.Find(examinationId);
+        }
+
+        public IEnumerable<Examination> GetExaminations()
+        {
+            return context.Examinations;
+        }
+
+
+        public void InsertExamination(Examination examination)
+        {
+            context.Examinations.Add(examination);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateExamination(Examination examination)
+        {
+            context.Entry(examination).State = System.Data.Entity.EntityState.Modified;
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+}
+}
