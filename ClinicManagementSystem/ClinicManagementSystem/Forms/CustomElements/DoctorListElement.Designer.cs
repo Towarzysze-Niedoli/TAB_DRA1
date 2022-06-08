b97cb69963a726c69558ce8f31bee0542f89c228
matadam667 <56017753+matadam667@users.noreply.github.com>
2022-06-08 14:08:00 +0200

    the only thing constant in life is change

--- ClinicManagementSystem/ClinicManagementSystem/Forms/CustomElements/DoctorListElement.Designer.cs
+++ ClinicManagementSystem/ClinicManagementSystem/Forms/CustomElements/DoctorListElement.Designer.cs
@@ -40,10 +40,10 @@ namespace ClinicManagementSystem.Forms.CustomElements
             // 
             this.pictureBox1.Enabled = false;
             this.pictureBox1.Image = global::ClinicManagementSystem.Properties.Resources.medical_symbol;
-            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(5, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(85, 85);
+            this.pictureBox1.Size = new System.Drawing.Size(106, 106);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -58,9 +58,9 @@ namespace ClinicManagementSystem.Forms.CustomElements
             this.UpperMainLabel.CausesValidation = false;
             this.UpperMainLabel.Enabled = false;
             this.UpperMainLabel.Font = new System.Drawing.Font("Corbel", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.UpperMainLabel.Location = new System.Drawing.Point(96, 4);
+            this.UpperMainLabel.Location = new System.Drawing.Point(120, 5);
             this.UpperMainLabel.Name = "UpperMainLabel";
-            this.UpperMainLabel.Size = new System.Drawing.Size(211, 37);
+            this.UpperMainLabel.Size = new System.Drawing.Size(264, 46);
             this.UpperMainLabel.TabIndex = 1;
             this.UpperMainLabel.Text = "UpperMainText";
             this.UpperMainLabel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListElement_MouseDown);
@@ -72,9 +72,9 @@ namespace ClinicManagementSystem.Forms.CustomElements
             this.BottomLabelOne.AutoEllipsis = true;
             this.BottomLabelOne.AutoSize = true;
             this.BottomLabelOne.CausesValidation = false;
-            this.BottomLabelOne.Location = new System.Drawing.Point(96, 57);
+            this.BottomLabelOne.Location = new System.Drawing.Point(120, 71);
             this.BottomLabelOne.Name = "BottomLabelOne";
-            this.BottomLabelOne.Size = new System.Drawing.Size(147, 24);
+            this.BottomLabelOne.Size = new System.Drawing.Size(184, 30);
             this.BottomLabelOne.TabIndex = 2;
             this.BottomLabelOne.Text = "BottomTextOne";
             this.BottomLabelOne.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListElement_MouseDown);
@@ -86,9 +86,9 @@ namespace ClinicManagementSystem.Forms.CustomElements
             this.BottomLabelTwo.AutoEllipsis = true;
             this.BottomLabelTwo.AutoSize = true;
             this.BottomLabelTwo.CausesValidation = false;
-            this.BottomLabelTwo.Location = new System.Drawing.Point(303, 57);
+            this.BottomLabelTwo.Location = new System.Drawing.Point(379, 71);
             this.BottomLabelTwo.Name = "BottomLabelTwo";
-            this.BottomLabelTwo.Size = new System.Drawing.Size(146, 24);
+            this.BottomLabelTwo.Size = new System.Drawing.Size(183, 30);
             this.BottomLabelTwo.TabIndex = 3;
             this.BottomLabelTwo.Text = "BottomTextTwo";
             this.BottomLabelTwo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListElement_MouseDown);
@@ -98,16 +98,16 @@ namespace ClinicManagementSystem.Forms.CustomElements
             // DoctorListElement
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(213)))), ((int)(((byte)(234)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(119)))), ((int)(((byte)(182)))));
             this.Controls.Add(this.BottomLabelTwo);
             this.Controls.Add(this.BottomLabelOne);
             this.Controls.Add(this.UpperMainLabel);
             this.Controls.Add(this.pictureBox1);
-            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "DoctorListElement";
-            this.Size = new System.Drawing.Size(526, 90);
+            this.Size = new System.Drawing.Size(658, 113);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.ListElement_MouseDown);
             this.MouseLeave += new System.EventHandler(this.ListElement_MouseLeave);
             this.MouseHover += new System.EventHandler(this.ListElement_MouseHover);
--- ClinicManagementSystem/ClinicManagementSystem/Forms/MainForms/VisitsMainForm.Designer.cs
+++ ClinicManagementSystem/ClinicManagementSystem/Forms/MainForms/VisitsMainForm.Designer.cs
@@ -43,6 +43,12 @@ namespace ClinicManagementSystem.Forms.MainForms
             this.PerformVisitButton = new System.Windows.Forms.Button();
             this.VisitStatusComboBox = new System.Windows.Forms.ComboBox();
             this.VisitDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // VisitsListPanel
@@ -227,12 +233,78 @@ namespace ClinicManagementSystem.Forms.MainForms
             this.VisitDatePicker.Size = new System.Drawing.Size(250, 27);
             this.VisitDatePicker.TabIndex = 26;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(959, 616);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 18);
+            this.label1.TabIndex = 27;
+            this.label1.Text = "Patient\'s Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(1283, 616);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 18);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Patient\'s Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(959, 692);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 18);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Doctor\'s Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(1283, 692);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 18);
+            this.label4.TabIndex = 30;
+            this.label4.Text = "Doctor\'s Surname";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(959, 768);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 18);
+            this.label5.TabIndex = 31;
+            this.label5.Text = "Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(1283, 768);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 18);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Time";
+            // 
             // VisitsMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(247)))), ((int)(((byte)(247)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1605, 1076);
+            this.ClientSize = new System.Drawing.Size(1605, 1061);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.VisitDatePicker);
             this.Controls.Add(this.VisitStatusComboBox);
             this.Controls.Add(this.PerformVisitButton);
@@ -272,5 +344,11 @@ namespace ClinicManagementSystem.Forms.MainForms
         private System.Windows.Forms.Button PerformVisitButton;
         public System.Windows.Forms.ComboBox VisitStatusComboBox;
         private System.Windows.Forms.DateTimePicker VisitDatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
